92c9844b0c45705495ff6ec96879583c08e5e375
josephsdavid <josephsd@smu.edu>
2023-03-13 10:09:16 -0500

    it lives

--- src/pythoncallmre.jl
+++ src/pythoncallmre.jl
@@ -5,7 +5,7 @@ export foo
 
 const foo = PythonCall.pynew() # initially NULL
 function __init__()
-    pth = dirname(@__DIR__) * "/py"        # Path where the python routines are
+    pth = joinpath(dirname(@__DIR__), "py")        # Path where the python routines are
     pyimport("sys").path.append(pth)  # append path
     PythonCall.pycopy!(foo, pyimport("foo"))
     return nothing
